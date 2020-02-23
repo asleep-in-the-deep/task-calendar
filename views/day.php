36c85902c644b999aae6aef55ab83a05a2933c30
Юрий Москов <aerobus.778@gmail.com>
2020-02-23 18:54:32 +0300

    фикс бага

--- views/day.php
+++ views/day.php
@@ -1,11 +1,15 @@
-<div class="day<?= $this->disabled?' disabled':'' ?>"><?php if (!$this->disabled) { ?>
-        <div class="day-button">Изменить</div><?php } ?>
-    <span class="day-number<?= isCurrentDay($this->currentDate)?' current':'' ?>"><?= $this->currentDay ?></span>
+<div class="day<?= $this->disabled?' disabled':'' ?>">
+	<?php if (!$this->disabled): ?>
+        <div class="day-button">Изменить</div>
+	<?php endif ?>
+    <span class="day-number<?= isCurrentDay($this->currentDate)?' current':'' ?>">
+		<?= $this->currentDay ?>
+	</span>
     <?php
     getTasks($this->currentDate);
-    if ($this->day['status'] == 1) { ?>
+    if ($this->day['status'] == 1): ?>
         <div class="finished"></div>
-    <?php } else { ?>
+    <?php elseif(!$this->disabled): ?>
         <div class="add-task"></div>
-    <?php } ?>
+    <?php endif ?>
 </div>
