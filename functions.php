16c7fe1bd27ed48607e019153cb5840bf531605e
Юрий Москов <aerobus.778@gmail.com>
2020-02-25 18:52:10 +0300

    Добавлены функции setStatusTask, changeColorTask, editCommentTask

--- functions.php
+++ functions.php
@@ -21,6 +21,15 @@ if (isset($_POST['function']) && !empty($_POST['function'])) {
         case 'renameTask':
             renameTask($_POST['id'], $_POST['title']);
             break;
+        case 'setStatusTask':
+            setStatusTask($_POST['id'], $_POST['status']);
+            break;
+        case 'changeColorTask':
+            changeColorTask($_POST['id'], $_POST['color']);
+            break;
+        case 'editCommentTask':
+            editCommentTask($_POST['id'], $_POST['comment']);
+            break;
     }
 }
 
@@ -36,6 +45,32 @@ function renameTask($id, $title) {
     $task = Task::get($id);
     if ($task !== null) {
         $task["title"] = $title;
+        $task->save();
+    }
+}
+
+function setStatusTask($id, $status) {
+    if (status == 0 || $status == 1) {
+        $task = Task::get($id);
+        if ($task !== null) {
+            $task["status"] = $status;
+            $task->save();
+        }
+    }
+}
+
+function editCommentTask($id, $comment) {
+    $task = Task::get($id);
+    if ($task !== null) {
+        $task["comment"] = $comment;
+        $task->save();
+    }
+}
+
+function changeColorTask($id, $color) {
+    $task = Task::get($id);
+    if ($task !== null) {
+        $task["color"] = $color;
         $task->save();
     }
 }
