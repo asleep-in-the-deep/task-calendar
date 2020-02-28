9b876724c906d9c388e2d6562756fc4af4c7d6b6
asleep-in-the-deep <47196327+asleep-in-the-deep@users.noreply.github.com>
2020-02-29 02:47:26 +0300

    data-date

--- views/day.php
+++ views/day.php
@@ -1,17 +1,17 @@
-<div class="day<?= $this->disabled?' disabled':'' ?>" id="day_<?= $this->currentDate ?>">
+<div class="day <?= $this->disabled?' disabled':'' ?>" data-date="<?= $this->currentDate ?>">
 	<?php if (!$this->disabled): ?>
         <div class="day-button">Изменить</div>
 	<?php endif ?>
     <span class="day-number<?= isCurrentDay($this->currentDate)?' current':'' ?>">
 		<?= $this->currentDay ?>
 	</span>
-	<div class="task-list">
-    <?php
-    getTasks($this->currentDate);
-    if ($this->day['status'] == 1): ?>
-        <div class="finished"></div>
-    <?php elseif(!$this->disabled): ?>
-        <div class="add-task"></div>
-    <?php endif ?>
+	<div class="task-group">
+	    <?php
+	    getTasks($this->currentDate);
+	    if ($this->day['status'] == 1): ?>
+		<div class="finished"></div>
+	    <?php elseif(!$this->disabled): ?>
+		<div class="add-task"></div>
+	    <?php endif ?>
 	</div>
 </div>
