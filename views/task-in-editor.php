2bff287467d61b0362280ef37bf26d2cc1044040
asleep-in-the-deep <47196327+asleep-in-the-deep@users.noreply.github.com>
2020-03-04 14:13:39 +0300

    Изменение вида задачи в попапе

--- views/task-in-editor.php
+++ views/task-in-editor.php
@@ -1,5 +1,5 @@
 <section id="task_<?= $this->task_id ?>" class="task  <?= $this->classname ?>">
-    <?= $this->title ?>
+    <span><?= $this->title ?></span>
     <div class="edit-task"></div>
-    <div class="delete-task"></div>
+    <div class="task-hours"><?= $this->hours ?></div>
 </section>
