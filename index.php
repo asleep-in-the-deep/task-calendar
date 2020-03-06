6e8010c539ba856e81b434ae15056fa04d11785f
Юрий Москов <aerobus.778@gmail.com>
2020-03-06 23:52:31 +0300

    Если отсутсвуют settings.json, config.php, calendar.db то они автоматически берутся из образцов.

--- index.php
+++ index.php
@@ -3,9 +3,22 @@ spl_autoload_register(function ($class) {
     include 'classes/' . str_replace('\\', '/', $class) . '.php';
 });
 
+CheckFileAndFix("config.php");
+CheckFileAndFix("calendar.db");
+CheckFileAndFix("settings.json");
+
 require_once 'config.php';
 require_once 'functions.php';
 
 require_once 'views/helpers.php';
 require_once 'views/template.php';
+
+function CheckFileAndFix($filename) {
+    if (!file_exists($filename)) {
+        if (file_exists("$filename.example")) {
+            copy("$filename.example", $filename);
+        }
+    }
+}
+
 ?>
